317d664b6ad4a5f4593e1a50c5d3c6e04a22ea31
Dominic Camill <camilldominic@gmail.com>
2025-09-08 11:53:52 -0700

    added more stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,38 @@
+'use client'
 /* eslint-disable @next/next/no-img-element */
 import styles from "./page.module.css";
 import Image from "next/image";
-import DinoBytes_icon from "@/public/DinoBytes_icon.png"
+import DinoBytes_icon from "@/public/DinoBytes_icon_light 512x512.png"
+import { useState } from "react";
+import { initFoodAPI, queryFood } from "@/lib/food";
 
 export default function Home() {
+	const [screen, setScreen] = useState<string>("Meal Planner")
+
+
 	return (
-		<div className={styles.page}>
+		<div className={`${styles.page} concert_one_regular`}>
 			<div className={styles.side_bar}>
 				<div className={styles.side_bar_title}>
-					<Image className={styles.side_bar_title_icon} src={DinoBytes_icon} alt="" />
+					<button title="Side bar collapse" type="button" className={styles.side_bar_title_icon_wrapper}>
+						<Image className={styles.side_bar_title_icon} src={DinoBytes_icon} alt="DinoBytes icons" />
+					</button>
 					<h1 className={`concert_one_regular ${styles.side_bar_title_text}`}>DinoBytes</h1>
 				</div>
 				<div className={styles.side_bar_redirect_list}>
-					<button className={`${styles.side_bar_redirect} concert_one_regular`}>
+					<button type="button" className={`${styles.side_bar_redirect} concert_one_regular`} onClick={() => { setScreen("Meal Planner"); queryFood(); }}>
 						<span className="material-symbols-rounded">
 							lunch_dining
 						</span>
 						Meal Planner
 					</button>
-					<button className={`${styles.side_bar_redirect} concert_one_regular`}>
+					<button type="button" className={`${styles.side_bar_redirect} concert_one_regular`} onClick={() => { setScreen("Budget Planner"); initFoodAPI(); }}>
 						<span className="material-symbols-rounded">
 							attach_money
 						</span>
 						Budget Planner
 					</button>
-					<button className={`${styles.side_bar_redirect} concert_one_regular`}>
+					<button type="button" className={`${styles.side_bar_redirect} concert_one_regular`} onClick={() => setScreen("Goal Planner")}>
 						<span className="material-symbols-rounded">
 							bookmark_flag
 						</span>
@@ -40,12 +48,17 @@ export default function Home() {
 					<div className={styles.side_bar_account_info}>
 						Name
 					</div>
-					<button className={styles.side_bar_account_edit}>
+					<button type="button" className={styles.side_bar_account_edit} onClick={() => setScreen("Settings")}>
 						<span className="material-symbols-rounded">
 							settings
 						</span>
 					</button>
 				</div>
+			</div>
+			<div className={styles.main}>
+				<h1 className={styles.main_title}>
+					{screen}
+				</h1>
 			</div>
 		</div>
 	);
---
+++ lib/food.ts
@@ -0,0 +1,26 @@
+const FOOD_API_URL = "https://world.openfoodfacts.net/"
+
+export function queryFood() {
+    fetch(FOOD_API_URL + "api/v2/products.json?search=chicken", {
+        method: 'GET',
+        headers: { Authorization: 'Basic ' + btoa('off:off') },
+    })
+        .then(req => {
+            if (!req.ok) {
+                return;
+            }
+            req.json();
+        })
+        .then(data => {
+            console.log(data)
+        })
+}
+
+export function initFoodAPI() {
+    fetch(FOOD_API_URL + "cgi/session.pl", {
+        method: 'GET',
+        headers: { Authorization: 'Basic ' + btoa('off:off') },
+    })
+        .then(req => req.json())
+        .then(json => console.log(json))
+}
