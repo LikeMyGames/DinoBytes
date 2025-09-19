7318616f448bcedbb7499f6f7318865670965929
DeclanWalsh589 <justdragonaround@icloud.com>
2025-09-19 18:48:39 +0000

    Continued working on budget planner (finished overall budget)

--- components/BudgetPlanner/BudgetPlanner.tsx
+++ components/BudgetPlanner/BudgetPlanner.tsx
@@ -8,32 +8,41 @@ export function BudgetPlanner() {
                 <h2>
                     Store
                 </h2>
+                <div className={style.store_info}>
+                    <h3>
+                        Address
+                    </h3>
+                </div>
             </div>
             <div className={style.budget_overall}>
                 <h2>
                     Budget
                 </h2>
+                <div>
+                    <div className={style.budget_info}>
+                        <h3>
+                            Weekly budget:
+                        </h3>
+
+                        <h3 className={style.budget_info_weekly}>
+                            $
+                        </h3>
+
+                    </div>
+                    <div className={style.budget_cost}>
+                        <h3>
+                            Total Weekly Cost:
+                        </h3>
+                        <h3 className={style.budget_cost_total}>
+                            $
+                        </h3>
+                    </div>
+                </div>
+
                 <button className={style.budget_change}>
                     <Icon iconName="add" />
                     <h4 className="concert_one_regular">Change budget</h4>
                 </button>
-                <div className={style.budget_info}>
-                    <h3>
-                        Weekly budget:
-                    </h3>
-                    
-                    <h3 className={style.budget_info_weekly}>
-                        $
-                    </h3>
-                </div>
-                <div className={style.budget_cost}>
-                    <h3>
-                        Total Weekly Cost:
-                    </h3>
-                    <h3 className={style.budget_cost_total}>
-                       $ 
-                    </h3>
-                </div>
             </div>
             <div className={style.budget_shopping}>
                 <h2>
