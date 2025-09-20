f52637b0fb9f09ee57d0f9d898653b1fbafd7d65
DeclanWalsh589 <justdragonaround@icloud.com>
2025-09-20 23:32:35 +0000

    Finished budget & store. Shopping list needs work (im finna chrashout)

--- components/BudgetPlanner/BudgetPlanner.tsx
+++ components/BudgetPlanner/BudgetPlanner.tsx
@@ -10,9 +10,21 @@ export function BudgetPlanner() {
                 </h2>
                 <div className={style.store_info}>
                     <h3>
-                        Address
+                        Address:
                     </h3>
+                    <ul className ={style.store_list}>
+                        <li>
+                            Bla blah blahhhh
+                        </li>
+                        <li>
+                            more of blashhhhh
+                        </li>
+                    </ul>
                 </div>
+                <button className={style.store_change}>
+                    <Icon iconName="add" />
+                    <h4 className="concert_one_regular">Change store</h4>
+                </button>
             </div>
             <div className={style.budget_overall}>
                 <h2>
@@ -33,10 +45,12 @@ export function BudgetPlanner() {
                         <h3>
                             Total Weekly Cost:
                         </h3>
-                        <h3 className={style.budget_cost_total}>
+
+                        
+                    </div>
+                    <h3 className={style.budget_cost_total}>
                             $
                         </h3>
-                    </div>
                 </div>
 
                 <button className={style.budget_change}>
@@ -45,10 +59,30 @@ export function BudgetPlanner() {
                 </button>
             </div>
             <div className={style.budget_shopping}>
-                <h2>
+                <h2 className={style.shopping_list}>
                     Shopping List
+
                 </h2>
+                <div className = {style.shopping_info}>
+                    <h3>
+                        Dairy:
+                    </h3>
+                    <ul className = {style.shopping_info_dairy}>
+                        
+                    </ul>
+                    <h3>
+                        Meat:
+                    </h3>
+                    <ul>
+                     
+                    </ul>
+                </div>
+                <button className={style.add_item}>
+                    <Icon iconName="add" />
+                    <h4 className="concert_one_regular">Add item</h4>
+                </button>
             </div>
+            
         </div>
     )
 }
