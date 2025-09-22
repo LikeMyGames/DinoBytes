b85bf98db20511ea3432b899d9225a8b59eb27f4
MCFan99 <maclex3983@yahoo.com>
2025-09-22 18:52:41 +0000

    Breakdown updated

--- components/Breakdown/Breakdown.tsx
+++ components/Breakdown/Breakdown.tsx
@@ -1,9 +1,17 @@
-import styles from "@/app/page.module.css"
+import styles from "./Breakdown.module.css"
 
 export function Breakdown() {
     return (
         <div className={styles.breakdown_container}>
-
+            <div className={styles.breakdown_calories}>
+                <h1>Calories:</h1>
+                <div className={styles.calories_count}>
+                    
+                </div>
+            </div>
+            <div className={styles.breakdown_budget_planner}>
+                <h1>Budget Planner</h1>
+            </div>
         </div>
     )
 }
