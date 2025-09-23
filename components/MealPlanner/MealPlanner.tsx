f818b13d41dd963d8f178d89ed0b622e12e3aac3
Dominic Camill <camilldominic@gmail.com>
2025-09-23 11:42:06 -0700

    added meal item adding popup

--- components/MealPlanner/MealPlanner.tsx
+++ components/MealPlanner/MealPlanner.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
-import styles from "./MealPlanner.module.css"
+import style from "./MealPlanner.module.css"
 // import { useState } from "react"
-import { ListsContext } from "@/app/page"
+import { Item, ListsContext } from "@/app/page"
 import { PieChart, Pie, Cell } from "recharts"
 import Icon from "@/components/Icon"
-import { useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
+
+const AddingItemContext = createContext<[boolean, (value: boolean) => void]>([false, () => { }])
 
 export default function MealPlanner() {
 	const [lists, setLists] = useContext(ListsContext)
+	const [addingItem, setAddingItem] = useState<boolean>(false)
+
 	let protein = 0
 	lists.breakfast?.map((item) => {
 		protein += item.protein
@@ -71,14 +75,14 @@ export default function MealPlanner() {
 	const RADIAN = Math.PI / 180
 
 	return (
-		<div className={styles.meal_container}>
-			<div className={styles.meal_date_container}>
+		<div className={`${style.meal_container} concert_one_regular`}>
+			<div className={style.meal_date_container}>
 
 			</div>
-			<div className={styles.meal_builder}>
-				<div className={styles.meal_breakdown}>
+			<div className={style.meal_builder}>
+				<div className={style.meal_breakdown}>
 					<h4>Breakdown</h4>
-					<PieChart width={200} height={200} className={styles.meal_breakdown_pie_container}>
+					<PieChart width={200} height={200} className={style.meal_breakdown_pie_container}>
 						<Pie data={data} legendType={"plainline"} labelLine={false} label={renderCustomizedLabel} dataKey={"value"} cx="50%" cy="50%" isAnimationActive={false}>
 							{data.map((entry, index) => (
 								<Cell key={`cell-${entry.name}`} fill={COLORS[index % COLORS.length]} />
@@ -86,85 +90,156 @@ export default function MealPlanner() {
 						</Pie>
 					</PieChart>
 				</div>
-				<div className={styles.meal_list_container}>
-					<div className={styles.meal_list}>
-						<h2 className={styles.meal_list_title}>
+				<div className={style.meal_list_container}>
+					<div className={style.meal_list}>
+						<h2 className={style.meal_list_title}>
 							Breakfast
 						</h2>
-						<div className={styles.meal_list_item_container}>
+						<div className={style.meal_list_item_container}>
 							{
 								lists.breakfast?.map((item, index) => (
-									<div className={styles.meal_list_item} key={index}>
+									<div className={style.meal_list_item} key={index}>
 
 									</div>
 								))
 							}
 
-							<div className={styles.meal_list_item}>
+							<div className={style.meal_list_item}>
 
 							</div>
 						</div>
-						<button className={styles.meal_list_add}>
+						<button className={style.meal_list_add} onClick={() => setAddingItem(true)}>
 							<Icon iconName="add" />
 							<h4 className="concert_one_regular">Add Item</h4>
 						</button>
 					</div>
-					<div className={styles.meal_list}>
-						<h2 className={styles.meal_list_title}>
+					<div className={style.meal_list}>
+						<h2 className={style.meal_list_title}>
 							Lunch
 						</h2>
-						<div className={styles.meal_list_item_container}>
-							<div className={styles.meal_list_item}>
+						<div className={style.meal_list_item_container}>
+							<div className={style.meal_list_item}>
 
 							</div>
-							<div className={styles.meal_list_item}>
+							<div className={style.meal_list_item}>
 
 							</div>
 						</div>
-						<button className={styles.meal_list_add}>
+						<button className={style.meal_list_add} onClick={() => setAddingItem(true)}>
 							<Icon iconName="add" />
 							<h4 className="concert_one_regular">Add Item</h4>
 						</button>
 					</div>
-					<div className={styles.meal_list}>
-						<h2 className={styles.meal_list_title}>
+					<div className={style.meal_list}>
+						<h2 className={style.meal_list_title}>
 							Dinner
 						</h2>
-						<div className={styles.meal_list_item_container}>
-							<div className={styles.meal_list_item}>
+						<div className={style.meal_list_item_container}>
+							<div className={style.meal_list_item}>
 
 							</div>
-							<div className={styles.meal_list_item}>
+							<div className={style.meal_list_item}>
 
 							</div>
 						</div>
-						<button className={styles.meal_list_add}>
+						<button className={style.meal_list_add} onClick={() => setAddingItem(true)}>
 							<Icon iconName="add" />
 							<h4 className="concert_one_regular">Add Item</h4>
 						</button>
 					</div>
-					<div className={styles.meal_list}>
-						<h2 className={styles.meal_list_title}>
+					<div className={style.meal_list}>
+						<h2 className={style.meal_list_title}>
 							Snack
 						</h2>
-						<div className={styles.meal_list_item_container}>
+						<div className={style.meal_list_item_container}>
 
 						</div>
-						<button className={styles.meal_list_add}>
+						<button className={style.meal_list_add} onClick={() => setAddingItem(true)}>
 							<Icon iconName="add" />
 							<h4 className="concert_one_regular">Add Item</h4>
 						</button>
 					</div>
 				</div>
 			</div>
+			<AddingItemContext.Provider value={[addingItem, setAddingItem]}>
+				<ItemSelect />
+			</AddingItemContext.Provider>
 		</div >
 	)
 }
 
-export function KrogerItemSelect() {
-	return (
-		<div className={styles.item_select}>
+export function ItemSelect() {
+	const [usePlanned, setUsePlanned] = useState<boolean>(false);
+	const [addingItem, setAddingItem] = useContext(AddingItemContext);
+	let searchQuery = ""
+	const [items, setItems] = useState<Item[]>([])
 
+	async function checkQuery() {
+		const oldQuery = searchQuery
+		setTimeout(() => {
+			if (oldQuery != searchQuery || oldQuery == "") {
+				return
+			}
+			console.log("querying kroger api")
+			// KrogerGetProducts(searchQuery);
+		}, 1000)
+	}
+
+	if (addingItem) {
+		return (
+			<div className={`${style.item_select} concert_one_regular`}>
+				<div className={style.item_select_card}>
+					<div className={`${style.item_select_type_select} concert_one_regular`}>
+						<button className="concert_one_regular" type="button" onClick={() => setUsePlanned(true)}>
+							Planned Meals
+						</button>
+						<button className="concert_one_regular" type="button" onClick={() => setUsePlanned(false)}>
+							Kroger Items
+						</button>
+						<button type="button" title="Close" className={style.item_select_type_select_close} onClick={() => setAddingItem(false)}>
+							<Icon iconName="close" />
+						</button>
+					</div>
+					<div className={style.item_select_chooser}>
+						{
+							usePlanned ? (
+								<></>
+							) : (
+								<>
+									<div className={style.item_select_chooser_search}>
+										<input className="concert_one_regular" title="Item Search" type="text"
+											onChange={(event) => {
+												event.preventDefault();
+												searchQuery = event.target.value;
+												console.log(searchQuery)
+												checkQuery();
+											}} />
+									</div>
+									<div className={style.item_select_chooser_container}>
+										{
+											items.map((val, i) => (
+												<ItemWidget key={i} item={val} />
+											))
+										}
+									</div>
+								</>
+							)
+						}
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export function MealBuilder() {
+
+}
+
+function ItemWidget({ item }: { item: Item }) {
+	return (
+		<div>
+			{item.name}
 		</div>
 	)
 }
