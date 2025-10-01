24515655ec2b73af7dcc30e15185e78d2e1fa9ea
MCFan99 <maclex3983@yahoo.com>
2025-10-01 18:44:48 +0000

    Created settings menu

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -1,9 +1,24 @@
 import style from "./Settings.module.css"
+import Icon from "@/components/Icon";
 
 export default function Settings() {
     return (
-        <div>
+        <div className={style.settings_container}>
+            <div className={style.settings_profile_info}>
+                <div className={style.profile_info_account_photo}>
+                    <Icon iconName="person" />
+                </div>
+                Name
+                <button className={`${style.profile_info_change_name} concert_one_regular`}>
+					Change
+				</button>
+            </div>
+            <div className={style.other}>
 
+            </div>
+            <div className={style.other1}>
+                
+            </div>
         </div>
     )
 }
