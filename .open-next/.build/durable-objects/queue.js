569b0e5bdcfb9bfa510aeaa947f38d89944f6521
Dominic Camill <camilldominic@gmail.com>
2025-10-24 11:49:15 -0700

    new build

--- .open-next/.build/durable-objects/queue.js
+++ .open-next/.build/durable-objects/queue.js
@@ -179,7 +179,7 @@ var DOQueueHandler = class extends DurableObject {
           "INSERT OR REPLACE INTO sync (id, lastSuccess, buildId) VALUES (?, unixepoch(), ?)",
           // We cannot use the deduplication id because it's not unique per route - every time a route is revalidated, the deduplication id is different.
           `${host}${url}`,
-          "f8ywcfQEHLE9uIt0WJ1hu"
+          "ECAYUD-lxgVD1i1PF6lTH"
         );
       }
       this.routeInFailedState.delete(msg.MessageDeduplicationId);
@@ -231,7 +231,7 @@ var DOQueueHandler = class extends DurableObject {
     }
     this.routeInFailedState.set(msg.MessageDeduplicationId, updatedFailedState);
     if (!this.disableSQLite) {
-      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "f8ywcfQEHLE9uIt0WJ1hu");
+      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "ECAYUD-lxgVD1i1PF6lTH");
     }
     await this.addAlarm();
   }
@@ -255,8 +255,8 @@ var DOQueueHandler = class extends DurableObject {
       return;
     this.sql.exec("CREATE TABLE IF NOT EXISTS failed_state (id TEXT PRIMARY KEY, data TEXT, buildId TEXT)");
     this.sql.exec("CREATE TABLE IF NOT EXISTS sync (id TEXT PRIMARY KEY, lastSuccess INTEGER, buildId TEXT)");
-    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "f8ywcfQEHLE9uIt0WJ1hu");
-    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "f8ywcfQEHLE9uIt0WJ1hu");
+    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "ECAYUD-lxgVD1i1PF6lTH");
+    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "ECAYUD-lxgVD1i1PF6lTH");
     const failedStateCursor = this.sql.exec("SELECT * FROM failed_state");
     for (const row of failedStateCursor) {
       this.routeInFailedState.set(row.id, JSON.parse(row.data));
--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1761324491487,
+    __BUILD_TIMESTAMP_MS__: 1761331672010,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
